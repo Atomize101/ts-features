319781dd6c3a10962d04f4d40dab58b6275cf68f
Chris Patrick <atomize101@gmail.com>
2021-01-18 12:09:11 -0500

    void and never

--- annotations/functions.ts
+++ annotations/functions.ts
@@ -22,3 +22,21 @@ function divide(a: number, b: number): number {
 const multiply = function (a: number, b: number): number {
 	return a * b;
 };
+
+const logger = (message: string): void => {
+	console.log(message);
+};
+
+// never means its never going to exit the function. This is pretty rare
+const throwError = (message: string): never => {
+	throw new Error(message);
+};
+
+// Usually it is like this
+const newThrowError = (message: string): string => {
+	if (!message) {
+		throw new Error(message);
+	}
+
+	return message;
+};

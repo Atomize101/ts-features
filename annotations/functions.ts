2fa8628f981d6a0b41717ecdae4e550b02eda87b
Chris Patrick <atomize101@gmail.com>
2021-01-18 11:50:50 -0500

    Creating the various functions with annotations

--- annotations/functions.ts
+++ annotations/functions.ts
@@ -1,3 +1,24 @@
+// Typescript function
 const add = (a: number, b: number): number => {
 	return a + b;
+};
+
+// Notice how Typescript tells us here that a return is missing from this function
+const subtract = (a: number, b: number): number => {
+	a - b;
+};
+
+// This is now the correct version
+const newSubtract = (a: number, b: number): number => {
+	return a - b;
+};
+
+// Here is a classic function wtih annotation
+function divide(a: number, b: number): number {
+	return a / b;
+}
+
+// Anonymous Function assigned to a variable with annotation
+const multiply = function (a: number, b: number): number {
+	return a * b;
 };

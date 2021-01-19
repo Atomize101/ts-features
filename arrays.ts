fe063289989e21b984a71db0241da7c0a8a0d426
Chris Patrick <atomize101@gmail.com>
2021-01-19 11:38:23 -0500

    Continuing Typescript with arrays

--- arrays.ts
+++ arrays.ts
@@ -7,3 +7,17 @@ const carsByMake = [['f150'], ['corolla'], ['camaro']];
 
 // Annotated empty 2d array
 const emptyCarsByMake: string[][] = [];
+
+// Help with inference when extracting values
+
+const car = carMakers[0];
+const myCar = carMakers.pop();
+
+// Prevent incompatible values
+carMakers.push(100);
+// Shows an error because the array is supposed to be strings.
+
+// Help with 'map'
+carMakers.map((car: string): string => {
+	return car.toUpperCase();
+});

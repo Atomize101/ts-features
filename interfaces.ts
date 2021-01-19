8fa3e4be644a97b5527108f6f63416079226da3a
Chris Patrick <atomize101@gmail.com>
2021-01-19 16:46:21 -0500

    Setting up simple Vehicle interface and show example of typescript error

--- interfaces.ts
+++ interfaces.ts
@@ -1,10 +1,18 @@
+interface Vehicle {
+	name: string;
+	year: number;
+	broken: boolean;
+}
+
 const oldCivic = {
 	name: 'civic',
 	year: 2000,
-	broken: true,
+	isBroken: true,
 };
 
-const printVehicle = (vehicle: { name: string; year: number; broken: boolean }): void => {
+// Now with the interface setup, if any of the values in oldCivic are changed, typescript will warn us
+
+const printVehicle = (vehicle: Vehicle): void => {
 	console.log(`Name: ${vehicle.name}`);
 	console.log(`Year: ${vehicle.year}`);
 	console.log(`Broken? ${vehicle.name}`);
